fd1beb6062581a7a0d96750ea51b01e60de76d83
Mayur Mane <mayur137137@gmail.com>
2020-12-02 19:04:54 +0530

    second last commit

--- cfg.py
+++ cfg.py
@@ -1,9 +1,10 @@
-from flask import Flask,abort,render_template,url_for, request, session, redirect,session
+from flask import Flask,abort,render_template,url_for, request,send_file, abort, session, redirect,session
 from flask_pymongo import PyMongo
 import json
 from datetime import datetime
 from hashlib import sha256
 import string
+import hashlib
 import random
 from numpy.ma.core import size
 import requests
@@ -11,11 +12,29 @@ import pymongo
 from boto import file, file
 import os
 from flask.debughelpers import DebugFilesKeyError
+from fileinput import filename
+import smtplib
+import urllib.request
+import webbrowser
+from pathlib import Path
+from datetime import datetime
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+from os import path
+from bson import ObjectId
+from pathlib import Path
+import smtplib 
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText 
+from email.mime.base import MIMEBase 
+from email import encoders 
 
-
+app = Flask(__name__)
 
 app.config["MONGO_URI"]="mongodb://localhost:27017/mycloud"
 app.config['UPLOAD_FOLDER'] = 'uploads'
 app.secret_key='bvsdhvbwbvubwuandsoowv'
-app = Flask(__name__)
 mongo=PyMongo(app)
